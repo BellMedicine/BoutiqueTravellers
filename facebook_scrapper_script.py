448cae430a4034ecc9923b4b6531437f1c9bd4c7
JuanPM <jua6n@yahoo.com>
2021-05-26 11:13:49 +0200

    Add TODOs, few comments, and a few cosmetic changes

--- facebook_scrapper_script.py
+++ facebook_scrapper_script.py
@@ -3,8 +3,13 @@ from facebook_scraper import get_posts
 import json
 import datetime
 
+# BoutiqueTravellers Group ID
 group_id = 106503496676216
 
+# Set number of posts wanted. Use -1 to try to get all
+n_posts = 2
+
+# Post items that are interesting to keep
 keep_items = [
     "post_id",
     "text",
@@ -22,7 +27,10 @@ keep_items = [
     "reactions",
 ]
 
+# Get group info from Facebook. Currently not used
+# TODO #2
 infos = get_group_info(group_id)
+
 posts = get_posts(
     group=group_id,
     page_limit=1,
@@ -31,26 +39,29 @@ posts = get_posts(
     options={"comments": True, "reactors": True},
 )
 
-i = 0
+i = 1
 post_list = []
 for post in posts:
     post = {item: post[item] for item in keep_items if item in post.keys()}
     post_list.append(post)
-    if i == 1:
+    if i == n_posts:
         break
     i += 1
 
 print("We got some Data")
 
+
 def myconverter(o):
     if isinstance(o, datetime.datetime):
         return o.__str__()
-    else: 
+    else:
         return o
 
-_=0
+
+_ = 0
+
+# TODO #3
 with open("2_posts.json", "w") as fout:
-    json.dump(post_list, fout, default = myconverter)
-    #json.dump(json.dumps(post_list, default = myconverter), fout)
+    json.dump(post_list, fout, default=myconverter)
 
 print("See you next time!")
